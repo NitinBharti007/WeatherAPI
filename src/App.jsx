e9f621c61f75cce93c67856b2f7a46d4046e4595
Nitin Bharti <jacksp198@gmail.com>
2024-12-21 00:30:55 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import CurrentWeather from './components/CurrentWeather'
-import FourDays from './components/FourDays'
-import SixteenDays from './components/SixteenDays'
+import React from 'react';
+import './App.css';
+import CurrentWeather from './components/CurrentWeather';
+import FourDays from './components/FourDays';
+import SixteenDays from './components/SixteenDays';
 
 const App = () => {
-  return (
-    <div>
-      <CurrentWeather/>
-      <FourDays/>
-      <SixteenDays/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="component-wrapper">
+                <CurrentWeather />
+            </div>
+            <div className="component-wrapper">
+                <FourDays />
+            </div>
+            <div className="component-wrapper">
+                <SixteenDays />
+            </div>
+        </div>
+    );
+};
 
-export default App
+export default App;
